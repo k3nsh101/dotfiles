c549abd2e650359c604a639300e88fde6c41f70b
Kenshi <samuduchanuka@gmail.com>
2024-10-27 23:38:39 +0530

    Initial commit

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+{
+	"williamboman/mason.nvim",
+	opts = {
+		ensure_installed = {
+			"lua-language-server", "stylua",
+			"html-lsp", "css-lsp" , "prettier",
+            "typescript-language-server", "csharp-language-server",
+            "texlab", "bash-language-server", "clangd",
+            "tailwindcss-language-server", "python-lsp-server"
+		},
+	},
+},
+
+{
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"vim", "lua", "vimdoc",
+            "html", "css", "javascript", "typescript",
+            "c_sharp", "python", "bash", "cpp"
+		},
+	},
+},
+    {
+        "lervag/vimtex",
+        config = function()
+            require "configs.latex"
+        end,
+    }
+}
