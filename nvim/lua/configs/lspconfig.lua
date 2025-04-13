754974ffd4399cc637d52cfd3e8db0d71afe32f3
Kenshi <samuduchanuka@gmail.com>
2025-04-13 12:19:57 +0530

    Add C++ lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "bashls", "csharp_ls", "texlab", "tailwindcss", "pylsp"}
+local servers = { "html", "cssls", "bashls", "csharp_ls", "texlab", "tailwindcss", "pylsp", "clangd"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
